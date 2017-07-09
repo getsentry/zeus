371d2919d666a5b8ecbb4017f116e9fd29ae178c
David Cramer <dcramer@gmail.com>
2017-07-08 21:05:21 -0700

    add shell command for ipython

--- setup.py
+++ setup.py
@@ -4,26 +4,33 @@ install_requires = [
     'blinker==1.4',
     'cached-property==1.3.0',
     'celery==4.0.2',
-    'exam==0.10.6',
-    'factory_boy==2.8.1',
-    'flake8==3.3.0',
     'flask==0.12.2',
     'flask-alembic==2.0.1',
     'flask-sqlalchemy==2.2',
     'honcho==1.0.1',
     'lxml==3.8.0',
     'marshmallow==2.13.5',
-    'oauth2cli\t==4.1.2',
+    'oauth2client==4.1.2',
     'psycopg2==2.7.1',
+    'raven==6.1.0',
+    'redis==2.10.5',
+    'requests==2.18.1',
+]
+
+dev_requires = [
+    'flake8==3.3.0',
+    'ipython==6.1.0',
+    'yapf==0.16.2',
+]
+
+test_requires = [
+    'exam==0.10.6',
+    'factory_boy==2.8.1',
     'pytest==3.1.2',
     'pytest-cov==2.5.1',
     'pytest-mock==1.6.0',
     'pytest-responses==0.1.0',
     'pytest-xdist==1.18.0',
-    'raven==6.1.0',
-    'redis==2.10.5',
-    'requests==2.18.1',
-    'yapf==0.16.2',
 ]
 
 setup(
@@ -35,5 +42,5 @@ setup(
         'console_scripts': ['zeus=zeus.cli:main'],
     },
     long_description=open('README.md').read(),
-    install_requires=install_requires,
+    install_requires=install_requires + dev_requires + test_requires,
 )
---
+++ zeus/cli/shell.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+from .base import cli
+
+
+@cli.command()
+def shell():
+    import IPython
+    from flask.globals import _app_ctx_stack
+    app = _app_ctx_stack.top.app
+    banner = 'Python %s on %s\nIPython: %s\nApp: %s%s\nInstance: %s\n' % (
+        sys.version, sys.platform, IPython.__version__, app.import_name, app.debug and ' [debug]' or
+        '', app.instance_path,
+    )
+
+    ctx = {}
+
+    startup = os.environ.get('PYTHONSTARTUP')
+    if startup and os.path.isfile(startup):
+        with open(startup, 'rb') as f:
+            eval(compile(f.read(), startup, 'exec'), ctx)
+
+    ctx.update(app.make_shell_context())
+
+    IPython.embed(banner1=banner, user_ns=ctx)
