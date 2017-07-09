522ceeb9614e52e1ffadf04b50e1b357f0a1fb12
David Cramer <dcramer@gmail.com>
2017-07-08 18:08:49 -0700

    add revision message to factory

--- zeus/factories/revision.py
+++ zeus/factories/revision.py
@@ -1,4 +1,5 @@
 import factory
+import factory.faker
 
 from zeus import models
 
@@ -7,6 +8,7 @@ class RevisionFactory(factory.Factory):
     sha = factory.Faker('sha1')
     repository = factory.SubFactory('zeus.factories.RepositoryFactory')
     repository_id = factory.SelfAttribute('repository.id')
+    message = factory.faker.Faker('sentence')
     date_created = factory.Faker('date_time')
     date_committed = factory.Faker('date_time')
 
