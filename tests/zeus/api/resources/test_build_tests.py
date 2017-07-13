2ccbe4f5c92df8b98f7194079e46efad4280f27c
David Cramer <dcramer@gmail.com>
2017-07-13 10:44:13 +0200

    add result filter to build tests

--- tests/zeus/api/resources/test_build_tests.py
+++ tests/zeus/api/resources/test_build_tests.py
@@ -22,3 +22,24 @@ def test_build_tests_list_empty(
     assert resp.status_code == 200
     data = resp.json()
     assert len(data) == 0
+
+
+def test_build_tests_list_result_filter(
+    client, default_login, default_repo, default_build, default_testcase, default_repo_access
+):
+    resp = client.get(
+        '/api/repos/{}/builds/{}/tests?result=failed'.
+        format(default_repo.name, default_build.number)
+    )
+    assert resp.status_code == 200
+    data = resp.json()
+    assert len(data) == 0
+
+    resp = client.get(
+        '/api/repos/{}/builds/{}/tests?result=passed'.
+        format(default_repo.name, default_build.number)
+    )
+    assert resp.status_code == 200
+    data = resp.json()
+    assert len(data) == 1
+    assert data[0]['id'] == str(default_testcase.id)
--- zeus/api/resources/build_tests.py
+++ zeus/api/resources/build_tests.py
@@ -1,5 +1,7 @@
+from flask import request
 from sqlalchemy.orm import contains_eager
 
+from zeus.constants import Result
 from zeus.models import Job, Build, TestCase
 
 from .base_build import BaseBuildResource
@@ -19,5 +21,12 @@ class BuildTestsResource(BaseBuildResource):
         ).filter(
             Job.build_id == build.id,
         )
+
+        result = request.args.get('result')
+        if result:
+            try:
+                query = query.filter(TestCase.result == getattr(Result, result))
+            except AttributeError:
+                raise NotImplementedError
 
         return self.respond_with_schema(testcases_schema, query)
