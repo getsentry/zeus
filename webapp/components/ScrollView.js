c95676d3fe78127b33e939c8f8af6d0262f2c437
David Cramer <dcramer@gmail.com>
2017-07-13 15:30:13 +0200

    fix scrollview (temp)

--- webapp/components/ScrollView.js
+++ webapp/components/ScrollView.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export default styled.div`
   position: absolute;
   top: 67px; /* TODO(ckj): calculate this dynamically */
-  left: 0;
+  left: 220px;
   right: 0;
   bottom: 0;
   overflow: auto;
