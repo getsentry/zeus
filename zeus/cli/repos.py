60458a53385e994f14a0ec6dbe45b099f25d1b2e
David Cramer <dcramer@gmail.com>
2017-07-14 16:32:04 +0200

    use name instead of url as primary hook

--- zeus/cli/repos.py
+++ zeus/cli/repos.py
@@ -3,7 +3,6 @@ import click
 from zeus.config import db
 from zeus.models import Repository, RepositoryAccess, RepositoryBackend, RepositoryProvider, RepositoryStatus, User
 from zeus.tasks import import_repo, sync_repo
-from zeus.utils.text import slugify
 
 from .base import cli
 
@@ -14,19 +13,18 @@ def repos():
 
 
 @repos.command()
-@click.argument('repository_url', required=True)
+@click.argument('repository_name', required=True)
 @click.option('--backend', default='git', type=click.Choice(['git']))
-@click.option('--name')
+@click.option('--url')
 @click.option('--active/--inactive', default=True)
-def add(repository_url, name, backend, active):
+def add(repository_name, url, backend, active):
     repo = Repository(
-        url=repository_url,
+        url=url,
+        name=repository_name,
         backend=getattr(RepositoryBackend, backend),
         status=RepositoryStatus.active if active else RepositoryStatus.inactive,
         provider=RepositoryProvider.native,
     )
-    if name:
-        repo.name = slugify(name)
     db.session.add(repo)
     db.session.commit()
 
@@ -36,10 +34,10 @@ def add(repository_url, name, backend, active):
 
 
 @repos.command()
-@click.argument('repository_url', required=True)
-def sync(repository_url):
+@click.argument('repository_name', required=True)
+def sync(repository_name):
     repo = Repository.query.unrestricted_unsafe().filter(
-        Repository.url == repository_url,
+        Repository.name == repository_name,
     ).first()
     sync_repo(repo_id=repo.id)
 
@@ -50,11 +48,11 @@ def access():
 
 
 @access.command('add')
-@click.argument('repository_url', required=True)
+@click.argument('repository_name', required=True)
 @click.argument('email', required=True)
-def access_add(repository_url, email):
+def access_add(repository_name, email):
     repo = Repository.query.unrestricted_unsafe().filter(
-        Repository.url == repository_url,
+        Repository.name == repository_name,
     ).first()
     user = User.query.filter(User.email == email).first()
     assert repo
