5fe22f44eaa1c30eaddb136a4b559654ea78443f
David Cramer <dcramer@gmail.com>
2017-08-15 11:41:26 -0700

    remove dupe NODE_ENV default

--- config/env.js
+++ config/env.js
@@ -62,7 +62,7 @@ function getClientEnvironment(publicUrl) {
   }, {
     // Useful for determining whether we’re running in production mode.
     // Most importantly, it switches React into the correct mode.
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: NODE_ENV,
     // Useful for resolving the correct path to static assets in `public`.
     // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
     // This should only be used as an escape hatch. Normally you would put
