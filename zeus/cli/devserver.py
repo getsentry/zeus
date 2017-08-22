eb3251a44296f03899976bbb6189de7c8bb7ffce
David Cramer <dcramer@gmail.com>
2017-08-21 22:27:00 -0700

    change default devserver port to 8080

--- zeus/cli/devserver.py
+++ zeus/cli/devserver.py
@@ -11,20 +11,24 @@ from .base import cli
 @cli.command()
 @click.option('--environment', default='development', help='The environment name.')
 @click.option('--workers/--no-workers', default=False)
-def devserver(environment, workers):
+@click.option('--port', '-p', default=8080)
+def devserver(environment, workers, port):
     os.environ.setdefault('FLASK_DEBUG', '1')
     os.environ['NODE_ENV'] = environment
 
     # TODO(dcramer): pass required attributes to 'run' directly instead
     # of relying on FLASK_DEBUG
     daemons = [
-        ('web', ['zeus', 'run']),
-        ('webpack', ['node_modules/.bin/webpack', '--watch', '--config=config/webpack.config.js']),
+        ('web', ['zeus', 'run', '--port={}'.format(port)]),
+        ('webpack', ['node_modules/.bin/webpack',
+                     '--watch', '--config=config/webpack.config.js']),
     ]
     if workers:
-        daemons.append(('worker', ['zeus', 'worker', '--cron', '--log-level=INFO']))
+        daemons.append(
+            ('worker', ['zeus', 'worker', '--cron', '--log-level=INFO']))
 
-    cwd = os.path.realpath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+    cwd = os.path.realpath(os.path.join(
+        os.path.dirname(__file__), os.pardir, os.pardir))
 
     manager = Manager()
     for name, cmd in daemons:
