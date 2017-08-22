f436cf21721e74f2cc7e1d30845703109a576020
David Cramer <dcramer@gmail.com>
2017-08-21 22:21:15 -0700

    generic webhook logging

--- zeus/web/hooks/base.py
+++ zeus/web/hooks/base.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request, Response
+from flask import current_app, jsonify, request, Response
 from flask.views import View
 from sqlalchemy.orm import joinedload
 
@@ -10,19 +10,24 @@ class BaseHook(View):
     methods = ['GET', 'POST', 'PUT', 'DELETE']
 
     def dispatch_request(self, hook_id, signature, *args, **kwargs) -> Response:
+        current_app.logger.info('received webhook id=%s', hook_id)
 
         hook = Hook.query.unrestricted_unsafe().options(
             joinedload('repository'),
         ).get(hook_id)
         if not hook.is_valid_signature(signature):
+            current_app.logger.warn('invalid webhook signature id=%s', hook_id)
             return '', 403
 
         try:
             method = getattr(self, request.method.lower())
         except AttributeError:
+            current_app.logger.warn(
+                'invalid webhook method id=%s, method=%s', hook_id, request.method)
             return self.respond({'message': 'resource not found'}, 405)
 
-        auth.set_current_tenant(auth.Tenant(repository_ids=[hook.repository_id]))
+        auth.set_current_tenant(auth.Tenant(
+            repository_ids=[hook.repository_id]))
 
         resp = method(hook, *args, **kwargs)
         if isinstance(resp, Response):
