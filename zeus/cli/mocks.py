8a606642d9ef8f239df2e8d7079ea4d130d78cb3
David Cramer <dcramer@gmail.com>
2017-09-05 20:53:04 -0700

    feat(mocks): Add stream command

--- zeus/cli/mocks.py
+++ zeus/cli/mocks.py
@@ -11,7 +11,7 @@ from zeus.tasks import aggregate_build_stats_for_job
 from .base import cli
 
 
-def mock_single_repository(builds=10, user_ids=()):
+def mock_single_repository(user_ids=()):
     repo = factories.RepositoryFactory.build(
         status=models.RepositoryStatus.active,
         github=True,
@@ -25,6 +25,7 @@ def mock_single_repository(builds=10, user_ids=()):
             models.Repository.owner_name == repo.owner_name,
             models.Repository.name == repo.name
         ).first()
+        click.echo('Using {!r}'.format(repo))
     else:
         click.echo('Created {!r}'.format(repo))
 
@@ -35,45 +36,48 @@ def mock_single_repository(builds=10, user_ids=()):
         })
 
     db.session.commit()
+    return repo
 
-    parent_revision = None
-    for n in range(builds):
-        revision = factories.RevisionFactory.create(
-            repository=repo,
-            parents=[parent_revision.sha] if parent_revision else None,
-        )
-        source = factories.SourceFactory.create(
-            revision=revision,
-            patch=factories.PatchFactory(
-                parent_revision=parent_revision,
-            ) if parent_revision and random() > 0.8 else None,
-        )
-        parent_revision = revision
-        build = factories.BuildFactory.create(source=source, travis=True)
-        click.echo('Created {!r}'.format(build))
 
-        for n in range(1, 4):
-            has_failure = randint(0, 2) == 0
-            job = factories.JobFactory.create(
-                build=build,
-                failed=has_failure,
-                passed=not has_failure,
-                travis=True,
+def mock_build(repo: models.Repository, parent_revision: models.Revision=None):
+    revision = factories.RevisionFactory.create(
+        repository=repo,
+        parents=[parent_revision.sha] if parent_revision else None,
+    )
+    source = factories.SourceFactory.create(
+        revision=revision,
+        patch=factories.PatchFactory(
+            parent_revision=parent_revision,
+        ) if parent_revision and random() > 0.8 else None,
+    )
+    parent_revision = revision
+    build = factories.BuildFactory.create(source=source, travis=True)
+    click.echo('Created {!r}'.format(build))
+
+    for n in range(1, 4):
+        has_failure = randint(0, 2) == 0
+        job = factories.JobFactory.create(
+            build=build,
+            failed=has_failure,
+            passed=not has_failure,
+            travis=True,
+        )
+
+        for n in range(randint(0, 50)):
+            test_failed = has_failure and randint(0, 5) == 0
+            factories.TestCaseFactory.create(
+                job=job,
+                failed=test_failed,
+                passed=not test_failed,
             )
-
-            for n in range(randint(0, 50)):
-                test_failed = has_failure and randint(0, 5) == 0
-                factories.TestCaseFactory.create(
-                    job=job,
-                    failed=test_failed,
-                    passed=not test_failed,
-                )
-            for n in range(randint(0, 50)):
-                factories.FileCoverageFactory.create(job=job, in_diff=randint(0, 5) == 0)
-            db.session.commit()
-            aggregate_build_stats_for_job(job_id=job.id)
-
+        for n in range(randint(0, 50)):
+            factories.FileCoverageFactory.create(
+                job=job, in_diff=randint(0, 5) == 0)
         db.session.commit()
+        aggregate_build_stats_for_job(job_id=job.id)
+
+    db.session.commit()
+    return build
 
 
 @cli.group('mocks')
@@ -85,4 +89,18 @@ def mocks():
 def load_all():
     user_ids = [u for u, in db.session.query(models.User.id)]
     for n in range(3):
-        mock_single_repository(user_ids=user_ids)
+        repo = mock_single_repository(user_ids=user_ids)
+        parent_revision = None
+        for n in range(10):
+            build = mock_build(repo, parent_revision)
+            parent_revision = build.source.revision
+
+
+@mocks.command()
+def stream():
+    user_ids = [u for u, in db.session.query(models.User.id)]
+    repo = mock_single_repository(user_ids=user_ids)
+    parent_revision = None
+    while True:
+        build = mock_build(repo, parent_revision)
+        parent_revision = build.source.revision
