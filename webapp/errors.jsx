2e5864e442970c3bbe4b3599d840014871aa08ad
David Cramer <dcramer@gmail.com>
2017-10-08 16:50:28 -0700

    fix: Attach code to ApiError

--- webapp/errors.jsx
+++ webapp/errors.jsx
@@ -1,4 +1,9 @@
-class ApiError extends Error {}
+class ApiError extends Error {
+  constructor(msg, code) {
+    super(msg);
+    this.code = code;
+  }
+}
 class ResourceNotFound extends Error {
   static code = 404;
 }
