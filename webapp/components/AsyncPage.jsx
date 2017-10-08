a9b95628e81c6d2f86393fe5423124b3587d53aa
David Cramer <dcramer@gmail.com>
2017-10-08 16:30:54 -0700

    fix: Improve error handling

--- webapp/components/AsyncPage.jsx
+++ webapp/components/AsyncPage.jsx
@@ -155,8 +155,7 @@ export default class AsyncPage extends Component {
   }
 
   renderError(error) {
-    throw Object.keys(this.state.errors).find();
-    // throw error;
+    throw Array.find(Object.keys(this.state.errors)) || error;
   }
 
   renderContent() {
