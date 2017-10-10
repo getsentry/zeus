fe1638f78889a098903ed4197d306d2f41d48f60
David Cramer <dcramer@gmail.com>
2017-10-10 00:03:00 -0700

    fix: Correct identity scope check

--- zeus/vcs/providers/github.py
+++ zeus/vcs/providers/github.py
@@ -63,7 +63,7 @@ class GitHubRepositoryProvider(RepositoryProvider):
             repo_data = github.get(
                 '/repos/{}/{}'.format(owner_name, repo_name))
         except ApiError as exc:
-            if exc.code == 404 and 'repo' not in identity:
+            if exc.code == 404 and 'repo' not in identity.config['scopes']:
                 raise IdentityNeedsUpgrade(
                     scope='repo',
                     identity=identity,
