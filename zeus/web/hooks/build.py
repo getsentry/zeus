513dd978114480ec08ba8a50d5ed3dfa66b858fb
David Cramer <dcramer@gmail.com>
2017-10-09 23:24:20 -0700

    fix: Add locks around build/job hooks

--- zeus/web/hooks/build.py
+++ zeus/web/hooks/build.py
@@ -1,5 +1,6 @@
 from flask import request
 
+from zeus.config import redis
 from zeus.models import Build
 from zeus.api import client
 
@@ -8,29 +9,34 @@ from .base import BaseHook
 
 class BuildHook(BaseHook):
     def post(self, hook, build_xid):
-        build = Build.query.filter(
-            Build.provider == hook.provider,
-            Build.external_id == build_xid,
-        ).first()
+        lock_key = 'hook:build:{hook_id}:{build_xid}'.format(
+            hook_id=hook.id.hex,
+            build_xid=build_xid,
+        )
+        with redis.lock(lock_key):
+            build = Build.query.filter(
+                Build.provider == hook.provider,
+                Build.external_id == build_xid,
+            ).first()
 
-        json = request.get_json() or {}
-        json['external_id'] = build_xid
-        json['provider'] = hook.provider
+            json = request.get_json() or {}
+            json['external_id'] = build_xid
+            json['provider'] = hook.provider
 
-        if build:
-            response = client.put(
-                '/repos/{}/builds/{}'.format(
-                    hook.repository.get_full_name(),
-                    build.number,
-                ),
-                json=json
-            )
-        else:
-            response = client.post(
-                '/repos/{}/builds'.format(
-                    hook.repository.get_full_name(),
-                ),
-                json=json
-            )
+            if build:
+                response = client.put(
+                    '/repos/{}/builds/{}'.format(
+                        hook.repository.get_full_name(),
+                        build.number,
+                    ),
+                    json=json
+                )
+            else:
+                response = client.post(
+                    '/repos/{}/builds'.format(
+                        hook.repository.get_full_name(),
+                    ),
+                    json=json
+                )
 
-        return response
+            return response
--- zeus/web/hooks/job.py
+++ zeus/web/hooks/job.py
@@ -1,5 +1,6 @@
 from flask import request
 
+from zeus.config import redis
 from zeus.models import Build, Job
 from zeus.api import client
 
@@ -15,32 +16,38 @@ class JobHook(BaseHook):
         if not build:
             return self.respond('', 404)
 
-        job = Job.query.filter(
-            Job.provider == hook.provider,
-            Job.external_id == job_xid,
-            Job.build_id == build.id,
-        ).first()
+        lock_key = 'hook:job:{hook_id}:{build_xid}:{job_xid}'.format(
+            hook_id=hook.id.hex,
+            build_xid=build_xid,
+            job_xid=job_xid,
+        )
+        with redis.lock(lock_key):
+            job = Job.query.filter(
+                Job.provider == hook.provider,
+                Job.external_id == job_xid,
+                Job.build_id == build.id,
+            ).first()
 
-        json = request.get_json() or {}
-        json['external_id'] = job_xid
-        json['provider'] = hook.provider
+            json = request.get_json() or {}
+            json['external_id'] = job_xid
+            json['provider'] = hook.provider
 
-        if job:
-            response = client.put(
-                '/repos/{}/builds/{}/jobs/{}'.format(
-                    hook.repository.get_full_name(),
-                    job.build.number,
-                    job.number,
-                ),
-                json=json
-            )
-        else:
-            response = client.post(
-                '/repos/{}/builds/{}/jobs'.format(
-                    hook.repository.get_full_name(),
-                    build.number,
-                ),
-                json=json
-            )
+            if job:
+                response = client.put(
+                    '/repos/{}/builds/{}/jobs/{}'.format(
+                        hook.repository.get_full_name(),
+                        job.build.number,
+                        job.number,
+                    ),
+                    json=json
+                )
+            else:
+                response = client.post(
+                    '/repos/{}/builds/{}/jobs'.format(
+                        hook.repository.get_full_name(),
+                        build.number,
+                    ),
+                    json=json
+                )
 
-        return response
+            return response
