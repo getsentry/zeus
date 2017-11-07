2dd1a1b842f96c32eae69d664aecb68776a08451
David Cramer <dcramer@gmail.com>
2017-11-06 19:45:13 -0800

    Avoid locking repository in build details

--- zeus/api/resources/base_build.py
+++ zeus/api/resources/base_build.py
@@ -10,9 +10,7 @@ class BaseBuildResource(Resource):
     def dispatch_request(
         self, provider: str, owner_name: str, repo_name: str, build_number: int, *args, **kwargs
     ) -> Response:
-        queryset = Build.query.options(
-            contains_eager('repository'),
-        ).join(Repository, Repository.id == Build.repository_id).filter(
+        queryset = Build.query.join(Repository, Repository.id == Build.repository_id).filter(
             Repository.provider == RepositoryProvider(provider),
             Repository.owner_name == owner_name,
             Repository.name == repo_name,
@@ -20,6 +18,12 @@ class BaseBuildResource(Resource):
         )
         if self.select_resurce_for_update():
             queryset = queryset.with_for_update()
+        else:
+            # HACK(dcramer): we dont want to lock the repo row, so for now just deal
+            # w/ the consequences of this
+            queryset = queryset.options(
+                contains_eager('repository'),
+            )
         build = queryset.first()
         if not build:
             return self.not_found()
