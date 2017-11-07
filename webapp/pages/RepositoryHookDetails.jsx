110e50e769f5e23bd9a1bb55573c5fcafb68076e
David Cramer <dcramer@gmail.com>
2017-11-06 23:15:07 -0800

    Be explicit in hook configuration

--- webapp/pages/RepositoryHookDetails.jsx
+++ webapp/pages/RepositoryHookDetails.jsx
@@ -14,7 +14,11 @@ const generateTravisConfig = publicHookBase => {
   webhooks:
     urls:
       - ${publicHookBase}/provider/travis/webhook
+    on_success: always
+    on_failure: always
     on_start: always
+    on_cancel: always
+    on_error: always
 install:
   - npm install zeus-cli
 after_success:
