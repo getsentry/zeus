beeada854c72af76e2e40a680c58d2da05e12c5b
David Cramer <dcramer@gmail.com>
2017-11-07 11:59:18 -0800

    Add config option to CLI

--- zeus/cli/repos.py
+++ zeus/cli/repos.py
@@ -1,7 +1,8 @@
 import click
 
 from zeus.config import db
-from zeus.models import (Repository, RepositoryAccess,
+from zeus.db.utils import create_or_update
+from zeus.models import (ItemOption, Repository, RepositoryAccess,
                          RepositoryBackend, RepositoryProvider, RepositoryStatus, User)
 from zeus.tasks import import_repo, sync_repo
 from zeus.utils.text import slugify
@@ -70,4 +71,49 @@ def access_add(repository, email):
     assert email
     access = RepositoryAccess(user=user, repository=repo)
     db.session.add(access)
+    db.session.commit()
+
+
+@repos.group('config')
+def config():
+    pass
+
+
+@config.command('get')
+@click.argument('repository', required=True)
+@click.argument('option', required=True, nargs=-1)
+def config_get(repository, option):
+    provider, owner_name, repo_name = repository.split('/', 2)
+    repo = Repository.query.unrestricted_unsafe().filter(
+        Repository.provider == RepositoryProvider(provider),
+        Repository.owner_name == owner_name,
+        Repository.name == repo_name,
+    ).first()
+
+    for key in option:
+        result = db.session.query(ItemOption.value).filter(
+            ItemOption.item_id == repo.id,
+            ItemOption.name == key
+        ).first()
+        click.echo('{} = {}'.format(key, result[0] if result else '(not set)'))
+
+
+@config.command('set')
+@click.argument('repository', required=True)
+@click.argument('option', required=True, nargs=-1)
+def config_set(repository, option):
+    provider, owner_name, repo_name = repository.split('/', 2)
+    repo = Repository.query.unrestricted_unsafe().filter(
+        Repository.provider == RepositoryProvider(provider),
+        Repository.owner_name == owner_name,
+        Repository.name == repo_name,
+    ).first()
+
+    for key, value in [o.split('=', 1) for o in option]:
+        create_or_update(ItemOption, where={
+            'item_id': repo.id,
+            'name': key,
+        }, values={
+            'value': value,
+        })
     db.session.commit()
