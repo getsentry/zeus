e42962c51b31c0b4e6bdc02f6ae1021bcbe0a191
Jan Michael Auer <jan.auer@sentry.io>
2017-11-08 19:48:19 -0800

    feat(cli): Add a cli script based on node

---
+++ cli/index.js
@@ -0,0 +1,54 @@
+/* eslint-env node */
+
+const _ = require('lodash');
+const request = require('request');
+
+const ZEUS_HOOK_BASE = process.env.ZEUS_HOOK_BASE;
+if (!ZEUS_HOOK_BASE) {
+  console.error('ERROR: Missing ZEUS_HOOK_BASE environment variable');
+  process.exit(1);
+}
+
+let CI_SYSTEM = null;
+let BUILD_ID = null;
+let JOB_ID = null;
+
+if (process.env.TRAVIS === 'true') {
+  CI_SYSTEM = 'travis';
+  BUILD_ID = process.env.TRAVIS_BUILD_ID;
+  JOB_ID = process.env.TRAVIS_JOB_ID;
+}
+
+if (process.env.APPVEYOR === 'True') {
+  CI_SYSTEM = 'appveyor';
+  BUILD_ID = process.env.APPVEYOR_BUILD_ID;
+  JOB_ID = process.env.APPVEYOR_JOB_ID;
+}
+
+if (!CI_SYSTEM) {
+  console.error('ERROR: No supported CI system detected');
+  process.exit(1);
+}
+
+require('yargs')
+  .demandCommand()
+  .command(
+    'upload <file> [type]',
+    'Upload a build artifact',
+    yargs =>
+      yargs
+        .positional('file', {
+          describe: 'Path to the artifact',
+          type: 'string'
+        })
+        .positional('type', {
+          describe: 'Mime type of the file to upload',
+          type: 'string'
+        }),
+    argv => {
+      const form = _.pick(argv, ['file', 'type']);
+      const url = `${ZEUS_HOOK_BASE}/builds/${BUILD_ID}/jobs/${JOB_ID}/artifacts`;
+      request.post(url).form(form);
+    }
+  )
+  .help().argv;
