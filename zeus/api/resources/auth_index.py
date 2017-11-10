43cc10fff32ef98522ba100da34816049908abb7
David Cramer <dcramer@gmail.com>
2017-11-10 13:47:48 -0500

    Add emails to auth details

--- zeus/api/resources/auth_index.py
+++ zeus/api/resources/auth_index.py
@@ -1,13 +1,14 @@
 from flask import session
 
 from zeus import auth
-from zeus.models import Identity, User
+from zeus.models import Email, Identity, User
 
 from .base import Resource
-from ..schemas import IdentitySchema, UserSchema
+from ..schemas import EmailSchema, IdentitySchema, UserSchema
 
-user_schema = UserSchema(strict=True)
+emails_schema = EmailSchema(many=True, strict=True)
 identities_schema = IdentitySchema(many=True, strict=True)
+user_schema = UserSchema(strict=True)
 
 
 class AuthIndexResource(Resource):
@@ -27,17 +28,20 @@ class AuthIndexResource(Resource):
         if user is None:
             context = {
                 'isAuthenticated': False,
-                'user': None,
-                'identities': [],
             }
         else:
             identity_list = list(Identity.query.filter(
                 Identity.user_id == user.id,
             ))
 
+            email_list = list(Email.query.filter(
+                Email.user_id == user.id,
+            ))
+
             context = {
                 'isAuthenticated': True,
                 'user': user_schema.dump(user).data,
+                'emails': emails_schema.dump(email_list).data,
                 'identities': identities_schema.dump(identity_list).data,
             }
         return context
