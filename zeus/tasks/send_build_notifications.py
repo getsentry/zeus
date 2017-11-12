020a41a73146abc80b43399085b43374f76de2bb
David Cramer <dcramer@gmail.com>
2017-11-12 15:38:55 -0800

    Missing tenant

--- zeus/tasks/send_build_notifications.py
+++ zeus/tasks/send_build_notifications.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 
+from zeus import auth
 from zeus.config import celery
 from zeus.constants import Result, Status
 from zeus.models import Build
@@ -9,7 +10,11 @@ from zeus.notifications import email
 @celery.task(name='zeus.tasks.send_build_notifications', max_retries=None)
 def send_build_notifications(build_id: UUID):
     build = Build.query.get(build_id)
-    assert build
+    if not build:
+        raise ValueError('Unable to find build with id = {}'.format(build_id))
+
+    auth.set_current_tenant(auth.Tenant(
+        repository_ids=[build.repository_id]))
 
     # double check that the build is still finished and only send when
     # its failing
