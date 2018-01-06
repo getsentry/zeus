6c8de1a08c72b198bc28f31733525dfe5dd22be5
David Cramer <dcramer@gmail.com>
2018-01-06 10:22:19 +0100

    test: Pin to UTC time on mock date

--- config/jest/date.js
+++ config/jest/date.js
@@ -1,4 +1,4 @@
 import MockDate from 'mockdate';
 
-const constantDate = new Date('2017-10-17T04:41:20'); //National Pasta Day
+const constantDate = new Date('2017-10-17T04:41:20Z'); //National Pasta Day
 MockDate.set(constantDate);
