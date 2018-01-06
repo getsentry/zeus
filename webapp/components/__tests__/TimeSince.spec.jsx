ccfc466688555adf7eb3b30b6d58c242eecb8bff
David Cramer <dcramer@gmail.com>
2018-01-06 10:14:17 +0100

    test: Correct timezone on TimeSince tests

--- webapp/components/__tests__/TimeSince.spec.jsx
+++ webapp/components/__tests__/TimeSince.spec.jsx
@@ -5,12 +5,12 @@ import TimeSince from '../TimeSince';
 
 describe('TimeSince', () => {
   it('renders default', () => {
-    const tree = render(<TimeSince date="2017-10-15T01:41:20" />);
+    const tree = render(<TimeSince date="2017-10-15T01:41:20Z" />);
     expect(tree).toMatchSnapshot();
   });
 
   it('renders 24 hour clock', () => {
-    const tree = render(<TimeSince date="2017-10-15T01:41:20" clock24Hours />);
+    const tree = render(<TimeSince date="2017-10-15T01:41:20Z" clock24Hours />);
     expect(tree).toMatchSnapshot();
   });
 });
