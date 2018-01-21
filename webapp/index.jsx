8b48f32635fb38de0f5ce3656601bcc4c9275983
David Cramer <dcramer@gmail.com>
2018-01-21 12:10:13 -0800

    ref: Disable auth from localStorage

--- webapp/index.jsx
+++ webapp/index.jsx
@@ -3,7 +3,7 @@ import {Router, browserHistory} from 'react-router';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 
-import {setAuth} from './actions/auth';
+// import {setAuth} from './actions/auth';
 
 import routes from './routes';
 import store from './store';
@@ -12,17 +12,19 @@ import 'react-select/dist/react-select.css';
 
 import './index.css';
 
+// TODO(dcramer): until we can resolve babel not compiling Error classes correctly
+// in prod this compounds issues
 // we cache the user details in localStorage, but its still fetched on
 // the initial load to update/validate
-let auth = localStorage.getItem('auth');
-if (auth) {
-  try {
-    store.dispatch(setAuth(JSON.parse(auth)));
-  } catch (ex) {
-    console.error(ex);
-    localStorage.removeItem('auth');
-  }
-}
+// let auth = localStorage.getItem('auth');
+// if (auth) {
+//   try {
+//     store.dispatch(setAuth(JSON.parse(auth)));
+//   } catch (ex) {
+//     console.error(ex);
+//     localStorage.removeItem('auth');
+//   }
+// }
 
 // import {registerLanguage} from 'react-syntax-highlighter/dist/light';
 // import diff from 'react-syntax-highlighter/dist/languages/diff';
