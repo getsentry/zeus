34e09e1471f510c5d3c9a7dcd24ee786b4f79505
David Cramer <dcramer@gmail.com>
2018-01-22 17:03:55 -0800

    fix: Correct handling of unknown revision

--- zeus/api/schemas/fields/revision.py
+++ zeus/api/schemas/fields/revision.py
@@ -14,6 +14,7 @@ class RevisionRefField(fields.Str):
                 revision = revisions.identify_revision(repo, value)
             except UnknownRevision as e:
                 current_app.logger.warn('invalid ref received', exc_info=True)
-                raise ValidationError from e
+                raise ValidationError(
+                    'unknown revision: {}'.format(value)) from e
             return revision.sha
         return value
