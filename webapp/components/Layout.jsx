a154e1faf63ea80cd12212b61511b8754db9f2c0
David Cramer <dcramer@gmail.com>
2018-07-24 13:30:39 +0100

    feat: Add footer with build/github

--- webapp/components/Layout.jsx
+++ webapp/components/Layout.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import Content from './Content';
 import Header from './Header';
+import Footer from './Footer';
 
 export default class Layout extends Component {
   static propTypes = {
@@ -14,6 +15,7 @@ export default class Layout extends Component {
       <div>
         <Header />
         <Content>{this.props.children}</Content>
+        <Footer />
       </div>
     );
   }
