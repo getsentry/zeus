aea557bde167e95b505a42877422a652baee14c9
Jan Michael Auer <account@jauer.org>
2018-08-15 14:35:42 +0200

    feat: Add a hook setting for travis domains (#135)

--- webapp/pages/RepositoryHookDetails.jsx
+++ webapp/pages/RepositoryHookDetails.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Select from 'react-select';
 
 import AsyncPage from '../components/AsyncPage';
 import Panel from '../components/Panel';
 import {ResultGrid, Column, Row} from '../components/ResultGrid';
 import SectionHeading from '../components/SectionHeading';
 import TimeSince from '../components/TimeSince';
+
+const TRAVIS_DOMAIN_OPTIONS = [
+  {value: 'api.travis-ci.org', label: 'travis-ci.org'},
+  {value: 'api.travis-ci.com', label: 'travis-ci.com'}
+];
 
 const generateTravisConfig = publicHookBase => {
   return `notifications:
@@ -43,6 +49,21 @@ export default class RepositoryHookDetails extends AsyncPage {
 
     return `${protocol}//${hostname}${port}${secret ? hook.secret_uri : hook.public_uri}`;
   }
+
+  onChangeDomain = selected => {
+    let {hook} = this.state;
+    let {params} = this.props;
+
+    let data = {
+      ...hook,
+      config: {
+        ...hook.config,
+        domain: selected.value
+      }
+    };
+
+    this.fetchDataForEndpoint('hook', `/hooks/${params.hookId}`, {method: 'PUT', data});
+  };
 
   renderBody() {
     let {hook} = this.state;
@@ -87,6 +108,22 @@ export default class RepositoryHookDetails extends AsyncPage {
               <TimeSince date={hook.created_at} />
             </Column>
           </Row>
+          {hook.provider.startsWith('travis') && (
+            <Row>
+              <Column width={200}>
+                <strong>Domain</strong>
+              </Column>
+              <OverflowColumn textAlign="left">
+                <Select
+                  placeholder="yoyoyo"
+                  clearable={false}
+                  options={TRAVIS_DOMAIN_OPTIONS}
+                  onChange={this.onChangeDomain}
+                  value={hook.config.domain || TRAVIS_DOMAIN_OPTIONS[0]}
+                />
+              </OverflowColumn>
+            </Row>
+          )}
         </ResultGrid>
 
         {hook.provider.startsWith('travis') && (
@@ -101,6 +138,10 @@ export default class RepositoryHookDetails extends AsyncPage {
     );
   }
 }
+
+const OverflowColumn = styled(Column)`
+  overflow: visible;
+`;
 
 const Config = styled.pre`
   padding: 0 20px;
