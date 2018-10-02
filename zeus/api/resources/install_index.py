b2d87946a4647555067add8255c24b8c40c74e9c
David Cramer <dcramer@gmail.com>
2018-10-01 17:32:02 -0700

    feat: Various improvements to statistics
    - Track 'active' users with new date_active field
    - Update all stats to be 'active' within 24h/30d
    - Add generic traffic hit counting to local process stats
    - Add process id
    - Add uptime
    - Add connections gauge

--- zeus/api/resources/install_index.py
+++ zeus/api/resources/install_index.py
@@ -1,7 +1,8 @@
 from datetime import timedelta
 from flask import current_app
 
-from zeus.models import Build, User, Repository
+from zeus.config import db, metrics
+from zeus.models import Build, User
 from zeus.utils import timezone
 
 from .base import Resource
@@ -14,7 +15,20 @@ class InstallIndexResource(Resource):
         """
         Return various details about the installation.
         """
+        one_day_ago = timezone.now() - timedelta(days=1)
+        thirty_days_ago = timezone.now() - timedelta(days=30)
+
         return {
+            "process": {
+                "id": str(metrics.guid),
+                "uptime": metrics.uptime,
+                "connections": metrics.connections.value,
+                "hits": {
+                    "5m": metrics.hits.count(300),
+                    "15m": metrics.hits.count(900),
+                    "1h": metrics.hits.count(3600),
+                },
+            },
             "config": {
                 "debug": current_app.debug,
                 "environment": current_app.config["SENTRY_ENVIRONMENT"],
@@ -22,13 +36,39 @@ class InstallIndexResource(Resource):
                 "pubsubEndpoint": current_app.config["PUBSUB_ENDPOINT"],
             },
             "stats": {
-                "buildsTotal": Build.query.unrestricted_unsafe().count(),
-                "builds24h": (
-                    Build.query.unrestricted_unsafe()
-                    .filter(Build.date_created > timezone.now() - timedelta(days=1))
-                    .count()
-                ),
-                "reposTotal": Repository.query.unrestricted_unsafe().count(),
-                "usersTotal": User.query.count(),
+                "builds": {
+                    "24h": (
+                        Build.query.unrestricted_unsafe()
+                        .filter(Build.date_created > one_day_ago)
+                        .count()
+                    ),
+                    "30d": (
+                        Build.query.unrestricted_unsafe()
+                        .filter(Build.date_created > thirty_days_ago)
+                        .count()
+                    ),
+                },
+                "repos": {
+                    "24h": (
+                        db.session.query(Build.repository_id)
+                        .distinct()
+                        .filter(Build.date_created > one_day_ago)
+                        .distinct()
+                        .count()
+                    ),
+                    "30d": (
+                        db.session.query(Build.repository_id)
+                        .distinct()
+                        .filter(Build.date_created > thirty_days_ago)
+                        .distinct()
+                        .count()
+                    ),
+                },
+                "users": {
+                    "24h": User.query.filter(User.date_active > one_day_ago).count(),
+                    "30d": User.query.filter(
+                        User.date_active > thirty_days_ago
+                    ).count(),
+                },
             },
         }
--- zeus/models/user.py
+++ zeus/models/user.py
@@ -1,6 +1,9 @@
+from sqlalchemy import func
+
 from zeus.config import db
 from zeus.db.mixins import StandardAttributes
 from zeus.db.utils import model_repr
+from zeus.utils import timezone
 
 
 class User(StandardAttributes, db.Model):
@@ -9,6 +12,13 @@ class User(StandardAttributes, db.Model):
     """
 
     email = db.Column(db.String(128), unique=True, nullable=False)
+    date_active = db.Column(
+        db.TIMESTAMP(timezone=True),
+        nullable=False,
+        default=timezone.now,
+        server_default=func.now(),
+        index=True,
+    )
 
     options = db.relationship(
         "ItemOption",
