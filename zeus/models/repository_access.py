456531906577feb0ead68a9f05fe5cef139281ab
David Cramer <dcramer@gmail.com>
2018-12-19 11:32:23 -0800

    ref: Remove unused repository_id index

--- zeus/models/repository_access.py
+++ zeus/models/repository_access.py
@@ -1,11 +1,13 @@
 from zeus.config import db
 from zeus.constants import Permission
-from zeus.db.mixins import RepositoryMixin
 from zeus.db.types import Enum, GUID
 from zeus.db.utils import model_repr
 
 
-class RepositoryAccess(RepositoryMixin, db.Model):
+class RepositoryAccess(db.Model):
+    repository_id = db.Column(
+        GUID, db.ForeignKey("repository.id", ondelete="CASCADE"), primary_key=True
+    )
     user_id = db.Column(
         GUID, db.ForeignKey("user.id", ondelete="CASCADE"), primary_key=True
     )
@@ -13,6 +15,7 @@ class RepositoryAccess(RepositoryMixin, db.Model):
         Enum(Permission), nullable=False, default=Permission.read, server_default="1"
     )
 
+    repository = db.relationship("Repository", innerjoin=True, uselist=False)
     user = db.relationship("User", innerjoin=True, uselist=False)
 
     __tablename__ = "repository_access"
