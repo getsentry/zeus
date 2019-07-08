8f2b10272b6a11a63bbb0bc0f8f689716225d8f3
Matt Robenolt <matt@ydekproductions.com>
2019-07-08 08:26:25 -0700

    Switch to mywsgi to remove boilerplate

--- zeus/cli/web.py
+++ zeus/cli/web.py
@@ -1,9 +1,5 @@
 import click
 import os
-import sys
-
-from subprocess import list2cmdline
-from honcho.manager import Manager
 
 from .base import cli
 
@@ -16,55 +12,18 @@ from .base import cli
 def web(host, port, processes, threads):
     os.environ["PYTHONUNBUFFERED"] = "true"
 
-    daemons = [
-        (
-            "web",
-            [
-                "uwsgi",
-                "--master",
-                "--vacuum",
-                "--enable-threads",
-                # "--lazy-apps",
-                "--single-interpreter",
-                "--http={}:{}".format(host, port),
-                "--processes={}".format(processes),
-                "--threads={}".format(threads),
-                "--log-x-forwarded-for",
-                "--buffer-size=32768",
-                "--post-buffering=65536",
-                "--need-app",
-                "--harakiri=120",
-                "--harakiri-verbose",
-                "--thunder-lock",
-                "--vacuum",
-                "--home={}".format(sys.prefix),
-                "--auto-procname",
-                "--procname-prefix-spaced=[zeus]",
-                "--module=zeus.app:app",
-                "--die-on-term",
-                "--ignore-sigpipe",
-                "--ignore-write-errors",
-                "--disable-write-exception",
-                # XXX(dcramer): this is disabled as our version of uwsgi doesnt
-                # know what it means
-                # '--wsgi-manage-chunked-input',
-            ],
-        )
-    ]
+    import mywsgi
 
-    cwd = os.path.realpath(
-        os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+    mywsgi.run(
+        "zeus.app:app",
+        "{}:{}".format(host, port),
+        processes=processes,
+        threads=threads,
+        buffer_size=32768,
+        post_buffering=65536,
+        harakiri=120,
+        harakiri_verbose=True,
+        # XXX(dcramer): this is disabled as our version of uwsgi doesnt
+        # know what it means
+        # wsgi_manage_chunked_input=True,
     )
-
-    manager = Manager()
-    for name, cmd in daemons:
-        manager.add_process(
-            name,
-            list2cmdline(cmd),
-            quiet=False,
-            cwd=cwd,
-            # env=os.environ,
-        )
-
-    manager.loop()
-    sys.exit(manager.returncode)
