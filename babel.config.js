6c991ed42199e4e8a9e8bc83cd8eceb9d3501374
David Cramer <dcramer@gmail.com>
2019-11-12 12:58:54 -0800

    Set default browserslist

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,8 @@ module.exports = function(api) {
   api.cache.using(() => process.env.NODE_ENV === 'development');
 
   return {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
+    presets: ['@babel/preset-env', '@babel/react'],
     plugins: [
-      '@babel/plugin-transform-runtime',
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-transform-runtime',
       'babel-plugin-idx'
