b568cf3bbb6e142e0c45ee8257adbf1a8d2f576f
David Cramer <dcramer@gmail.com>
2019-11-11 18:47:42 -0800

    more attempted fixes

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
 module.exports = function(api) {
-  api.cache(true);
+  api.cache.using(() => process.env.NODE_ENV === 'development');
 
   return {
     presets: ['@babel/preset-env', '@babel/preset-react'],
     plugins: [
+      '@babel/plugin-transform-runtime',
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-transform-runtime',
       'babel-plugin-idx'
