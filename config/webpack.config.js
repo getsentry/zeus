02e66603ea6d9d12d98876edad7bec41f9d3f142
David Cramer <dcramer@gmail.com>
2019-11-13 09:58:18 -0800

    Remove uglify plugin (obsolete)

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -68,27 +68,6 @@ function getPlugins() {
     })
   );
 
-  // Minify the code.
-  isProd &&
-    plugins.push(
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false,
-          // Disabled because of an issue with Uglify breaking seemingly valid code:
-          // https://github.com/facebookincubator/create-react-app/issues/2376
-          // Pending further investigation:
-          // https://github.com/mishoo/UglifyJS2/issues/2011
-          comparisons: false
-        },
-        output: {
-          comments: false,
-          // Turned on because emoji and regex is not minified properly using default
-          // https://github.com/facebookincubator/create-react-app/issues/2488
-          ascii_only: true
-        },
-        sourceMap: true
-      })
-    );
   return plugins;
 }
 
