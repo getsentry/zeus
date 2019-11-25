48d26efd007807f2fccbe680ae339d97520519e1
David Cramer <dcramer@gmail.com>
2019-11-25 11:54:05 -0800

    Fix js stubs

--- config/jest/testStubs.js
+++ config/jest/testStubs.js
@@ -92,41 +92,52 @@ window.TestStubs = {
     ...params
   }),
 
-  Build: params => ({
-    created_at: '2018-01-06T16:07:16.830829+00:00',
-    external_id: '325812408',
-    finished_at: '2018-01-06T16:11:04.393590+00:00',
-    id: 'aa7097a2-f2fb-11e7-a565-0a580a28057d',
-    label: 'fix: Remove break-word behavior on coverage',
-    number: 650,
-    provider: 'travis',
-    result: 'passed',
-    started_at: '2018-01-06T16:07:16.957093+00:00',
-    stats: {
-      coverage: {
-        diff_lines_covered: 0,
-        diff_lines_uncovered: 0,
-        lines_covered: 6127,
-        lines_uncovered: 3060
+  Build: params => {
+    let author = (params || {}).author || TestStubs.Author();
+    return {
+      created_at: '2018-01-06T16:07:16.830829+00:00',
+      external_id: '325812408',
+      finished_at: '2018-01-06T16:11:04.393590+00:00',
+      id: 'aa7097a2-f2fb-11e7-a565-0a580a28057d',
+      label: 'fix: Remove break-word behavior on coverage',
+      number: 650,
+      provider: 'travis',
+      result: 'passed',
+      started_at: '2018-01-06T16:07:16.957093+00:00',
+      author: author,
+      revision: {
+        author: author,
+        committed_at: '2018-01-06T16:06:52+00:00',
+        created_at: '2018-01-06T16:06:52+00:00',
+        message: 'fix: Remove break-word behavior on coverage\n',
+        sha: 'eff634a68a01d081c0bdc51752dfa0709781f0e4'
       },
-      style_violations: {
-        count: 0
+      stats: {
+        coverage: {
+          diff_lines_covered: 0,
+          diff_lines_uncovered: 0,
+          lines_covered: 6127,
+          lines_uncovered: 3060
+        },
+        style_violations: {
+          count: 0
+        },
+        tests: {
+          count: 153,
+          count_unique: 153,
+          duration: 14673.0,
+          failures: 0,
+          failures_unique: 0
+        },
+        webpack: {
+          total_asset_size: 0
+        }
       },
-      tests: {
-        count: 153,
-        count_unique: 153,
-        duration: 14673.0,
-        failures: 0,
-        failures_unique: 0
-      },
-      webpack: {
-        total_asset_size: 0
-      }
-    },
-    status: 'finished',
-    url: 'https://travis-ci.org/getsentry/zeus/builds/325812408',
-    ...params
-  }),
+      status: 'finished',
+      url: 'https://travis-ci.org/getsentry/zeus/builds/325812408',
+      ...params
+    };
+  },
 
   Email: params => ({
     id: '659dc21c-81db-11e7-988a-0a580a28047a',
@@ -160,20 +171,6 @@ window.TestStubs = {
     latest_build: TestStubs.Build({repository: undefined}),
     message: 'fix: Maintain repository in published data\n',
     sha: '26fef62489212d56d0a5037e3e6d876b887e972b',
-    ...params
-  }),
-
-  Source: params => ({
-    author: TestStubs.Author(),
-    created_at: '2018-01-06T16:07:16.814650+00:00',
-    id: 'aa6e1f90-f2fb-11e7-a565-0a580a28057d',
-    revision: {
-      author: TestStubs.Author(),
-      committed_at: '2018-01-06T16:06:52+00:00',
-      created_at: '2018-01-06T16:06:52+00:00',
-      message: 'fix: Remove break-word behavior on coverage\n',
-      sha: 'eff634a68a01d081c0bdc51752dfa0709781f0e4'
-    },
     ...params
   }),
 
