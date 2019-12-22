225dbc5542d7755de983cb4b629dcf0cceeece46
David Cramer <dcramer@gmail.com>
2019-12-22 10:00:21 -0800

    English

--- webapp/pages/RepositorySettings.jsx
+++ webapp/pages/RepositorySettings.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import AsyncPage from '../components/AsyncPage';
 import Label from '../components/Label';
 import Section from '../components/Section';
 import SectionHeading from '../components/SectionHeading';
 
-import {addIndicator, removeIndicator} from '../actions/indicators';
+import { addIndicator, removeIndicator } from '../actions/indicators';
 
 class RepositorySettings extends AsyncPage {
   getEndpoint() {
-    let {provider, ownerName, repoName} = this.props.params;
+    let { provider, ownerName, repoName } = this.props.params;
     return `/repos/${provider}/${ownerName}/${repoName}`;
   }
 
@@ -19,7 +19,7 @@ class RepositorySettings extends AsyncPage {
   }
 
   togglePublic = () => {
-    let {repo} = this.state;
+    let { repo } = this.state;
     let indicator = this.props.addIndicator('Saving changes..', 'loading');
     this.api
       .put(this.getEndpoint(), {
@@ -30,7 +30,7 @@ class RepositorySettings extends AsyncPage {
       .then(result => {
         this.props.removeIndicator(indicator);
         this.props.addIndicator('Changes saved!', 'success', 5000);
-        this.setState({repo: result});
+        this.setState({ repo: result });
       })
       .catch(error => {
         this.props.addIndicator('An error occurred.', 'error', 5000);
@@ -40,14 +40,14 @@ class RepositorySettings extends AsyncPage {
   };
 
   renderBody() {
-    let {repo} = this.state;
+    let { repo } = this.state;
     return (
       <div>
         <SectionHeading>Settings</SectionHeading>
         <Section>
           <Label>
             <input type="checkbox" checked={repo.public} onChange={this.togglePublic} />{' '}
-            {'Allow any authenticated user read-access to this repository in Zeus'}
+            {'Allow any authenticated user read-only access to this repository in Zeus'}
           </Label>
         </Section>
       </div>
