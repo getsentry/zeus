32b68be26c76fc7a308f46b8493f5dd198e49ab8
David Cramer <dcramer@gmail.com>
2020-01-03 17:37:44 -0800

    fix: Remove duplicate process_pending_artifacts

--- zeus/tasks/cleanup_artifacts.py
+++ zeus/tasks/cleanup_artifacts.py
@@ -4,12 +4,10 @@ from sqlalchemy import or_
 
 from zeus.config import celery, db
 from zeus.constants import Status
-from zeus.exceptions import UnknownJob
-from zeus.models import Artifact, Build, PendingArtifact
+from zeus.models import Artifact
 from zeus.utils import timezone
 
 from .process_artifact import process_artifact
-from .process_pending_artifact import process_pending_artifact
 
 
 @celery.task(name="zeus.cleanup_artifacts", time_limit=300)
@@ -54,26 +52,3 @@ def cleanup_artifacts(task_limit=100):
             "cleanup: process_artifact %s [processed]", result.id
         )
         process_artifact(artifact_id=result.id)
-
-
-@celery.task(name="zeus.cleanup_pending_artifacts", time_limit=300)
-def cleanup_pending_artifacts(task_limit=100):
-    # find any pending artifacts which seemingly are stuck (not enqueued)
-    queryset = (
-        db.session.query(PendingArtifact.id)
-        .filter(
-            Build.external_id == PendingArtifact.external_build_id,
-            Build.provider == PendingArtifact.provider,
-            Build.repository_id == PendingArtifact.repository_id,
-            Build.status == Status.finished,
-        )
-        .limit(task_limit)
-    )
-
-    for result in queryset:
-        current_app.logger.warning("cleanup: process_pending_artifact %s", result.id)
-        try:
-            process_pending_artifact(pending_artifact_id=result.id)
-        except UnknownJob:
-            # do we just axe it?
-            pass
