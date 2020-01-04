f86b6771b03346c5d33da27f3761e0607349ba23
David Cramer <dcramer@gmail.com>
2020-01-03 17:29:49 -0800

    ref: Log when repos are disabled

--- zeus/utils/repos.py
+++ zeus/utils/repos.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from uuid import UUID
 
 from zeus.config import db
@@ -7,8 +8,10 @@ from zeus.models import ItemOption, Repository, RepositoryStatus
 def disable_repo(repository_id: UUID, repository: Repository = None):
     if repository:
         repository.status = RepositoryStatus.inactive
-    ItemOption.query.filter(
-        ItemOption.item_id == repository_id, ItemOption.name == "auth.private-key"
-    ).delete()
-    db.session.add(repository)
+    with db.session.begin_nested():
+        ItemOption.query.filter(
+            ItemOption.item_id == repository_id, ItemOption.name == "auth.private-key"
+        ).delete()
+        db.session.add(repository)
+    current_app.logger.warn('Deactivated repository %s', repository_id)
     db.session.commit()
