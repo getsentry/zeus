da65770bfef017a8ca61829ca80d7535753e6548
David Cramer <dcramer@gmail.com>
2020-02-19 10:10:55 -0800

    Update Flask to 1.0 and improve CLI
    - Refactor our repo parsing and query
    - Support looking up repo by UUID

--- zeus/cli/repos.py
+++ zeus/cli/repos.py
@@ -20,6 +20,29 @@ from zeus.vcs.utils import get_vcs
 from .base import cli
 
 
+from flask.cli import with_appcontext
+
+
+@with_appcontext
+def RepositoryType(value):
+    if "/" in value:
+        provider, owner_name, repo_name = value.split("/", 2)
+        result = (
+            Repository.query.unrestricted_unsafe()
+            .filter_by(
+                provider=RepositoryProvider(provider),
+                owner_name=owner_name,
+                name=repo_name,
+            )
+            .first()
+        )
+    else:
+        result = Repository.query.unrestricted_unsafe().get(value)
+
+    assert result
+    return result
+
+
 @cli.group("repos")
 def repos():
     pass
@@ -52,23 +75,12 @@ def access():
 
 
 @access.command("add")
-@click.argument("repository", required=True)
+@click.argument("repository", type=RepositoryType, required=True)
 @click.argument("email", required=True)
 def access_add(repository, email):
-    provider, owner_name, repo_name = repository.split("/", 2)
-    repo = (
-        Repository.query.unrestricted_unsafe()
-        .filter(
-            Repository.provider == RepositoryProvider(provider),
-            Repository.owner_name == owner_name,
-            Repository.name == repo_name,
-        )
-        .first()
-    )
     user = User.query.filter(User.email == email).first()
-    assert repo
     assert email
-    access = RepositoryAccess(user=user, repository=repo)
+    access = RepositoryAccess(user=user, repository=repository)
     db.session.add(access)
     db.session.commit()
 
@@ -79,82 +91,53 @@ def config():
 
 
 @config.command("get")
-@click.argument("repository", required=True)
+@click.argument("repository", type=RepositoryType, required=True)
 @click.argument("option", required=True, nargs=-1)
 def config_get(repository, option):
-    provider, owner_name, repo_name = repository.split("/", 2)
-    repo = (
-        Repository.query.unrestricted_unsafe()
-        .filter(
-            Repository.provider == RepositoryProvider(provider),
-            Repository.owner_name == owner_name,
-            Repository.name == repo_name,
-        )
-        .first()
-    )
-
     for key in option:
         result = (
             db.session.query(ItemOption.value)
-            .filter(ItemOption.item_id == repo.id, ItemOption.name == key)
+            .filter(ItemOption.item_id == repository.id, ItemOption.name == key)
             .first()
         )
         click.echo("{} = {}".format(key, result[0] if result else "(not set)"))
 
 
 @config.command("set")
-@click.argument("repository", required=True)
+@click.argument("repository", type=RepositoryType, required=True)
 @click.argument("option", required=True, nargs=-1)
 def config_set(repository, option):
-    provider, owner_name, repo_name = repository.split("/", 2)
-    repo = (
-        Repository.query.unrestricted_unsafe()
-        .filter(
-            Repository.provider == RepositoryProvider(provider),
-            Repository.owner_name == owner_name,
-            Repository.name == repo_name,
-        )
-        .first()
-    )
-
     for key, value in [o.split("=", 1) for o in option]:
         create_or_update(
-            ItemOption, where={"item_id": repo.id, "name": key}, values={"value": value}
+            ItemOption,
+            where={"item_id": repository.id, "name": key},
+            values={"value": value},
         )
     db.session.commit()
 
 
 @repos.command()
-@click.argument("repository", required=True)
+@click.argument("repository", type=RepositoryType, required=True)
 @click.argument("parent", default="master", required=False)
 @click.option("--local", default=False, is_flag=True, required=False)
 @click.option("--limit", default=100, required=False)
 @coroutine
 async def log(repository, parent, local, limit):
-    provider, owner_name, repo_name = repository.split("/", 2)
-    repo = (
-        Repository.query.unrestricted_unsafe()
-        .filter(
-            Repository.provider == RepositoryProvider(provider),
-            Repository.owner_name == owner_name,
-            Repository.name == repo_name,
-        )
-        .first()
-    )
-
     if local:
         db_pool = await create_db_pool()
 
         async with db_pool.acquire() as conn:
-            vcs = await get_vcs(conn, repo.id)
+            vcs = await get_vcs(conn, repository.id)
 
         results = vcs.log(parent=parent, limit=limit)
 
         for entry in results:
             click.echo(f"{entry.sha}\n  {entry.author}")
     else:
-        tenant = auth.RepositoryTenant(repo.id)
-        results = vcs_client.log(repo.id, parent=parent, limit=limit, tenant=tenant)
+        tenant = auth.RepositoryTenant(repository.id)
+        results = vcs_client.log(
+            repository.id, parent=parent, limit=limit, tenant=tenant
+        )
 
         for entry in results:
             click.echo(
