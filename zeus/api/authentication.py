0924edbaeed9f5fdee42f886e15d0f92b059c953
David Cramer <dcramer@gmail.com>
2020-05-26 09:00:47 -0700

    feat: Add span for authentication mechanism

--- zeus/api/authentication.py
+++ zeus/api/authentication.py
@@ -1,14 +1,17 @@
 from flask import request
 
 from zeus import auth
+from zeus.utils.sentry import span
 
 
 class HeaderAuthentication(object):
+    @span("auth.header")
     def authenticate(self):
         return auth.get_tenant_from_headers(request.headers)
 
 
 class SessionAuthentication(object):
+    @span("auth.session")
     def authenticate(self):
         user = auth.get_current_user()
         if not user:
